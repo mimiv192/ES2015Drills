540559c8babd21e78803f274ab2bee9dd835dbf4
mimiv192 <misha.vernon@yahoo.com>
2018-09-27 14:58:31 -0500

    ES2015Drills

---
+++ ES2015.js
@@ -0,0 +1,56 @@
+function favMovie(movie) {
+        this.movie=Anchorman;
+        console.log(`my favorite movie is ${Anchorman}`)
+    }
+        return 
+    movie.favMovie.call("The Room");{
+    }
+        return `(my favorite movie is ${call.movie}`);
+    
+    
+    movie.favMovie.call(movie);
+    
+    let name = Misha;
+        console.log(`my name is ${name} and my favorite movie is ${movie}`);
+    }
+    function favMovie (movie,name){
+        this.movie = Anchorman;
+        this.name = Misha;
+    }
+    
+    //number 5
+    getFirstName (fullName) => {
+        console.log(fullName);
+        return fullName.split('')[0];
+    
+    
+    };
+    console.log(getFirstName('Misha Vernon'));
+    getFirstName('Misha')
+    let getFirstNameConcise = name => fullName.split(' ')[0];
+    console.log(getFirstNameConcise('Misha Vernon'));
+    
+    //spread
+    let name= 'misha';
+    let logValues = (str)=> {
+        let arr = [...str];
+      
+        arr.forEach((s)=> {
+            console.log(s);
+        });
+    };  
+    logValues(name);
+    
+    // notes 
+    let possibleDogNames = (dogOwner, ...dogNames) => {
+    console.log(dogOwner)
+    console.log(dogsNames)
+    };
+    possibleDogNames('me','motorchops','lily','dude','muffin','chicken')
+    
+    let myFavoriteCatNames = ['catface','kat','quasimodo','luke'];
+    let moreCatNames = ['dog','powderball','valium'];
+    let copied = [...myFavoriteCatNames,...moreCatNames]
+    console.log(copied);
+    
+    
